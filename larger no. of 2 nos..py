35189ef5e959be14f2bc097d2293e913bbaf5271
anishmanchanda <anishmanchanda2006@gmail.com>
2022-08-25 14:41:26 +0530

    Create larger no. of 2 nos..py

---
+++ larger no. of 2 nos..py
@@ -0,0 +1,8 @@
+#To print larger number of 2 inputted numbers
+a= int(input("Enter number 1:"))
+b= int(input("Enter number 2"))
+
+if a>=b:
+    print("The largest number is:",a)
+else:
+    print("The largest number is:",b)
